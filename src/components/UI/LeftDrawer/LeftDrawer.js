dbe1c35858fc3fe69e77f101eb77d088d53cadc5
Tiago Taquelim <sikozonbatata@gmail.com>
2019-05-16 15:38:06 +0100

    fullscreen and image index margin fixed

--- src/components/UI/LeftDrawer/LeftDrawer.js
+++ src/components/UI/LeftDrawer/LeftDrawer.js
@@ -1,84 +1,79 @@
-import React from 'react';
+import React from "react";
 
-import { withStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
+import { withStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
 
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import Hd from '@material-ui/icons/Hd';
-import Favorite from '@material-ui/icons/Favorite';
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import Hd from "@material-ui/icons/Hd";
+import Favorite from "@material-ui/icons/Favorite";
 import classes from "./LeftDrawer.module.css";
-import Dashboard from '@material-ui/icons/Dashboard';
-import Person from '@material-ui/icons/Person';
-import ListItemText from '@material-ui/core/ListItemText';
-import Switch from '@material-ui/core/Switch';
+import Dashboard from "@material-ui/icons/Dashboard";
+import Person from "@material-ui/icons/Person";
+import ListItemText from "@material-ui/core/ListItemText";
+import Switch from "@material-ui/core/Switch";
 
 const styles = {
-  list: {
-    width: 250,
-  },
-  fullList: {
-    width: 'auto',
-  },
+	list: {
+		width: 250,
+	},
+	fullList: {
+		width: "auto",
+	},
 };
 
 const leftDrawer = (props) => {
+	const sideList = (
+		<div className={classes.list}>
+			<List>
+				<ListItem>
+					<h3>AstroPicture</h3>
+				</ListItem>
+				<Divider />
+				<ListItem button>
+					<Hd />
+					<ListItemText primary="Hd Images" />
+					<Switch
+						checked={props.isHD}
+						onChange={props.changeResolution}
+						value={props.isHD}
+					/>
+				</ListItem>
+				<ListItem button>
+					{/* NOTE: when using router see: https://material-ui.com/guides/composition/#react-routers */}
+					<Favorite />
+					<ListItemText primary="Favorites" />
+				</ListItem>
+				<ListItem button>
+					<Dashboard />
+					<ListItemText primary="Change to grid" />
+				</ListItem>
+				<Divider />
+				<ListItem button>
+					<Person />
+					<ListItemText primary="About" />
+				</ListItem>
+			</List>
+			<Divider />
+			<List />
+		</div>
+	);
 
-    const sideList = (
-      <div className={classes.list}>
-        <List>
-          <ListItem >
-          <h3>APOD</h3>
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <Hd />
-            <ListItemText primary="Hd Images"/>
-            <Switch
-                checked={props.isHD}
-                onChange={props.changeResolution}
-                value={props.isHD}
-              />
-          </ListItem>
-          <ListItem button>
-            {/* NOTE: when using router see: https://material-ui.com/guides/composition/#react-routers */}
-            <Favorite/>
-            <ListItemText primary="Favorites"/>
-          </ListItem>
-          <ListItem button>
-            <Dashboard/>
-            <ListItemText primary="Change to grid"/>
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <Person/>
-            <ListItemText primary="About"/>
-          </ListItem>
-        </List>
-        <Divider />
-        <List>
-
-        </List>
-      </div>
-    );
-
-
-    return (
-      <div>
-        <Drawer open={props.isOpen} onClose={props.close}>
-          <div
-            tabIndex={0}
-            role="button"
-            onClick={props.close}
-            onKeyDown={props.close}
-          >
-            {sideList}
-          </div>
-        </Drawer>
-      </div>
-    );
-}
-
+	return (
+		<div>
+			<Drawer open={props.isOpen} onClose={props.close}>
+				<div
+					tabIndex={0}
+					role="button"
+					onClick={props.close}
+					onKeyDown={props.close}
+				>
+					{sideList}
+				</div>
+			</Drawer>
+		</div>
+	);
+};
 
 export default withStyles(styles)(leftDrawer);
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,199 +1,224 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import classes from './App.module.css';
+import classes from "./App.module.css";
 
 import Spinner from "../components/UI/Spinner/Spinner";
-import Navbar from "../components/Navbar/Navbar"
+import Navbar from "../components/Navbar/Navbar";
 import Controls from "../components/Controls/Controls";
-import ImageInfo from "../components/ImageInfo/ImageInfo"
+import ImageInfo from "../components/ImageInfo/ImageInfo";
 import ToggleButton from "../components/UI/ToggleButton/ToggleButton";
 import LeftDrawer from "../components/UI/LeftDrawer/LeftDrawer";
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';  
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 const APIDATA = {
-  baseUrl: "https://api.nasa.gov/planetary/apod",
-  queryParams: function( date ) {
-    return "?start_date=2019-02-01&end_date=" + date
-  },
-  key: "&api_key=EOr3djWLSSP1bfFu3QKBOleBAaG2h1qDzaBfQSU2",
-  url: function( todayDate ) {
-    return this.baseUrl + this.queryParams( todayDate ) + this.key
-  }
-}
+	baseUrl: "https://api.nasa.gov/planetary/apod",
+	queryParams: function(date) {
+		return "?start_date=2019-02-01&end_date=" + date;
+	},
+	key: "&api_key=EOr3djWLSSP1bfFu3QKBOleBAaG2h1qDzaBfQSU2",
+	url: function(todayDate) {
+		return this.baseUrl + this.queryParams(todayDate) + this.key;
+	},
+};
 
 const theme = createMuiTheme({
-  palette: {
-     primary: {
-        main: '#fff'
-
-     },
-     secondary: {
-       main: 'rgb(255, 157, 0)',
-     },
-     light: {
-        main: 'rgba(255, 255, 255, 0.485)'
-     }
-  },
-  typography: { 
-     useNextVariants: true
-  }
+	palette: {
+		primary: {
+			main: "#fff",
+		},
+		secondary: {
+			main: "rgb(255, 157, 0)",
+		},
+		light: {
+			main: "rgba(255, 255, 255, 0.485)",
+		},
+	},
+	typography: {
+		useNextVariants: true,
+	},
 });
-
 
 //TODO: Lazy load images after 1 month and be able to navigate the whole time date
 //TODO: Add random section
 class App extends Component {
-  state = {
-    data: [],
-    loading: true,
-    currImg: {},
-    currHDimg: null,
-    index: 0,
-    infoHidden: false,
-    drawer: false,
-    hd: false
-  }
+	state = {
+		data: [],
+		loading: true,
+		currImg: {},
+		currHDimg: null,
+		index: 0,
+		infoHidden: false,
+		drawer: false,
+		hd: false,
+		fullScreen: false,
+	};
 
-  componentDidMount() {
+	componentDidMount() {
+		// Used to fetch
+		const date = new Date();
+		const year = date.getFullYear();
+		const month = Number(date.getMonth()) + 1;
+		const day = date.getDate();
+		const currDayAndMonth = `${year}-${str_pad(month)}-${str_pad(day)}`;
 
-    // Used to fetch
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = Number(date.getMonth()) + 1 ;
-    const day = date.getDate();
-    const currDayAndMonth =  `${year}-${str_pad(month)}-${str_pad(day)}` ;
+		fetch(APIDATA.url(currDayAndMonth))
+			.then((res) => res.json())
+			.then((data) => {
+				this.setState({
+					data: data.reverse(),
+					currImg: {
+						...data[0],
+					},
+					currHDimg: data[0].hdurl,
+					loading: false,
+				});
+				console.log(data);
+			})
+			.catch((err) => console.error(err));
+	}
 
-    fetch( APIDATA.url( currDayAndMonth ) )
-      .then( res => res.json() )
-      .then( data => {
-        this.setState({
-           data: data.reverse(),
-           currImg: {
-             ...data[0]
-            }, 
-           currHDimg: data[0].hdurl,
-           loading:false
-           });
-        console.log(data)
-      } )
-    .catch( err => console.error(err) );
-  }
+	fullScreenHandler = () => {
+		this.setState({ fullScreen: !this.state.fullScreen });
+	};
 
+	changeImageHandler = (action) => {
+		let _index = 0;
 
-  changeImageHandler = (action) => {
-    let _index = 0;
-    // TODO: Add error handeling
+		if (action === "next") {
+			_index = this.state.index + 1;
+		}
+		if (action === "back" && this.state.index > 0) {
+			_index = this.state.index - 1;
+		}
+		this.setState({
+			index: _index,
+			currImg: {
+				...this.state.data[_index],
+			},
+			currHDimg: this.state.data[_index].hdurl
+				? this.state.data[_index].hdurl
+				: this.state.data[_index].url,
+		});
+	};
 
-    if(action === "next"){
-      _index = this.state.index +1;
+	hdImagesHandler = () => {
+		const oldVal = this.state.hd;
+		this.setState({ hd: !oldVal });
+	};
 
-    } else {
-      _index = this.state.index -1;
-    }
-    this.setState({ 
-      index: _index,
-      currImg: {
-        ...this.state.data[_index]
-      },
-      currHDimg: this.state.data[_index].hdurl 
-      ? this.state.data[_index].hdurl
-       : this.state.data[_index].url
-    });
-  }
+	infoBarHandler = () => {
+		const infoHidden = this.state.infoHidden;
+		this.setState({ infoHidden: !infoHidden });
+	};
 
-  hdImagesHandler = () => {
-    const oldVal = this.state.hd;
-    this.setState( {hd: !oldVal} );
-  }
+	changeResolution = () => {
+		const oldVal = this.state.hd;
+		this.setState({ hd: !oldVal });
+	};
 
-  infoBarHandler = () => {
-    const infoHidden = this.state.infoHidden;
-    this.setState( {infoHidden: !infoHidden} );
-  }
+	drawerHandler = () => {
+		const oldVal = this.state.drawer;
+		this.setState({ drawer: !oldVal });
+	};
 
-  changeResolution = () => {
-    const oldVal = this.state.hd;
-    this.setState( {hd: !oldVal} );
-  }
+	// Converts input from API "2018-05-01" to a more human friendly one
+	convertDateFormat = (date) => {
+		const arr = date.split("-");
+		const month = MONTHS[parseInt(arr[1], 10) - 1].val;
+		return `${month} ${arr[2]}, ${arr[0]}`;
+	};
 
-  drawerHandler = () => {
-    const oldVal = this.state.drawer;
-    this.setState( {drawer: !oldVal} )
-  }
+	render() {
+		let InfoComponent = null;
+		if (this.state.currImg.date) {
+			InfoComponent = this.state.infoHidden ? (
+				<ToggleButton
+					type="arrowUp"
+					clicked={this.infoBarHandler}
+					label={"Show Bar"}
+				/>
+			) : (
+				<ImageInfo
+					style={{
+						display: this.state.fullScreen ? "none" : "inherit",
+					}}
+					data={this.state.currImg}
+					formatedDate={this.convertDateFormat(
+						this.state.currImg.date
+					)}
+					hideBar={this.infoBarHandler}
+				/>
+			);
+		}
 
-  // Converts input from API "2018-05-01" to a more human friendly one
-  convertDateFormat = (date) => {
-    const arr = date.split("-");
-    const month = MONTHS[parseInt(arr[1], 10) - 1].val;
-    return `${month} ${arr[2]}, ${arr[0]}`;
-  }
+		const currImgToDisplay = this.state.hd
+			? this.state.currHDimg
+			: this.state.currImg.url;
 
-  render() {
+		return (
+			<MuiThemeProvider theme={theme}>
+				<div
+					className={classes.App}
+					style={{ backgroundImage: `url(${currImgToDisplay})` }}
+				>
+					<LeftDrawer
+						isOpen={this.state.drawer}
+						close={this.drawerHandler}
+						changeResolution={this.changeResolution}
+						hdImages={this.hdImagesHandler}
+						isHD={this.state.hd}
+					/>
 
-    let InfoComponent = null;
-    if(this.state.currImg.date ){
-       InfoComponent = (
-        this.state.infoHidden ?
-         <ToggleButton type="arrowUp" clicked={this.infoBarHandler} label={"Show Bar"} /> 
-         : <ImageInfo 
-			data={this.state.currImg}
-			formatedDate={this.convertDateFormat(this.state.currImg.date)}
-			hideBar={this.infoBarHandler}
-          />  
-      );
-    }
+					{this.state.loading ? <Spinner /> : null}
 
-    const currImgToDisplay = this.state.hd ? this.state.currHDimg : this.state.currImg.url;
+					<h2
+						className={classes.Title}
+						style={{
+							display: this.state.fullScreen ? "none" : "inherit",
+						}}
+					>
+						{this.state.currImg.title}
+					</h2>
 
-    return (
-      <MuiThemeProvider theme = { theme }>
-        <div className={classes.App} style={{backgroundImage: `url(${currImgToDisplay})`}}>
-          <LeftDrawer
-             isOpen={this.state.drawer} 
-             close={this.drawerHandler}
-             changeResolution={this.changeResolution}
-             hdImages={this.hdImagesHandler}
-             isHD={this.state.hd} />
+					<Navbar
+						openDrawer={this.drawerHandler}
+						style={{
+							display: this.state.fullScreen ? "none" : "inherit",
+						}}
+					/>
 
-          { this.state.loading ? <Spinner /> : null}
+					{InfoComponent}
 
-          <h2 className={classes.Title}>{this.state.currImg.title}</h2>
-
-          <Navbar 
-            openDrawer={this.drawerHandler} />
-
-          {InfoComponent}
-  
-          <Controls 
-            currImg={currImgToDisplay} 
-            date={this.state.currImg.url} 
-            changeImage={this.changeImageHandler} />
-        </div>
-      </MuiThemeProvider>
-    );
-  }
+					<Controls
+						changeFullscreen={this.fullScreenHandler}
+						currImg={currImgToDisplay}
+						date={this.state.currImg.url}
+						changeImage={this.changeImageHandler}
+					/>
+				</div>
+			</MuiThemeProvider>
+		);
+	}
 }
-
-
 
 // helper function for month formating
 function str_pad(n) {
-  return String("00" + n).slice(-2);
+	return String("00" + n).slice(-2);
 }
 
 const MONTHS = [
-  {"val": "January"},
-  {"val": "February"},
-  {"val": "March"},
-  {"val": "April"},
-  {"val": "May"},
-  {"val": "June"},
-  {"val": "July"},
-  {"val": "August"},
-  {"val": "September"},
-  {"val": "October"},
-  {"val": "November"},
-  {"val": "December"}
- ];
+	{ val: "January" },
+	{ val: "February" },
+	{ val: "March" },
+	{ val: "April" },
+	{ val: "May" },
+	{ val: "June" },
+	{ val: "July" },
+	{ val: "August" },
+	{ val: "September" },
+	{ val: "October" },
+	{ val: "November" },
+	{ val: "December" },
+];
 
 export default App;
